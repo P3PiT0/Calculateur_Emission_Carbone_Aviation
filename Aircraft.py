5a9bebeb6738e77de758a76b23d8439febea4cbf
P3PiT0 <lucas.chz@laposte.net>
2023-06-11 10:58:57 -0400

    Structure classes Aircraft et Airline

---
+++ Aircraft.py
@@ -0,0 +1,14 @@
+class Aircraft(): 
+    '''
+    Docstring
+    '''
+    #Attribut
+    
+    #Dictionnaire de concordance Avion/Moteur
+    
+    
+    #Constructeur 
+    def __init__(self,):
+        '''Constructeur 
+        '''
+    #Méthodes 
---
+++ Airline.py
@@ -0,0 +1,12 @@
+class Airline(): 
+    '''
+    Docstring
+    '''
+    
+    #Attribut
+    
+    #Constructeur 
+    def __init__(self,):
+        '''Constructeur 
+        '''
+    #Méthodes 
