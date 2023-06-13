870ef5cbc9bec14100c27221b121c39a05ccc72f
C16-H34 <133154131+C16-H34@users.noreply.github.com>
2023-06-13 14:32:23 -0400

    Ajout de la structure de base pour la documentation Sphinx

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Calculateur_Emission_Carbone_Aviation'
+copyright = '2023, Lucas Chamaz lucas.chamaz.1@ens.etsmtl.ca ; Alexandre Panhaleux alenxandre.panhaleux.1@ens.etsmtl.ca ; Etienne Lionnet etienne.lionnet.1@ens.etsmtl.ca ; Gabriel Gunther gabriel.gunther.1@ens.etsmtl.ca'
+author = 'Lucas Chamaz lucas.chamaz.1@ens.etsmtl.ca ; Alexandre Panhaleux alenxandre.panhaleux.1@ens.etsmtl.ca ; Etienne Lionnet etienne.lionnet.1@ens.etsmtl.ca ; Gabriel Gunther gabriel.gunther.1@ens.etsmtl.ca'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../other_code"))
+
+extensions = [« sphinx.ext.autodoc »]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
