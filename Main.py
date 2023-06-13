d13758ffcbb18d19db8e9a1dd38de5c43029bb92
alexandrephl <alex.panhaleux@orange.fr>
2023-06-13 15:00:34 -0400

    Fin Yaml

--- Main.py
+++ Main.py
@@ -3,35 +3,50 @@ import Engine
 import Aircraft
 import Airline 
 import Travel
+import Lecteuryamel
+import sys
 
 #Formatage du dataframe
 df=Data_Reader.Emission_Data_Reader()
 
 
 #Création de mon objet moteur et affichage de sa consommation
-moteur = Engine.Moteur('GE90-76B',df)
+#moteur = Engine.Moteur('GE90-76B',df)
 
 #Création de mon objet moteur 
-avion = Aircraft.Aircraft('AIRBUS 300 B4600')
+#avion = Aircraft.Aircraft('AIRBUS 300 B4600')
 
 #Création d'un voyage entre deux aéroports 
 voyage = Travel.travel('BHX', 'CVT')
 
 #TEST MOTEUR
-print(f"Information de mon moteur : {moteur.df_moteur}")
-print(f"Emission de CO2 lors de la phase LTO : {moteur.equivalent_carbone_LTO} g")
-print(f"Emission de CO2 lors de la phase de croisière : {moteur.equivalent_carbone_seconde_cruise} g/s")
+#print(f"Information de mon moteur : {moteur.df_moteur}")
+#print(f"Emission de CO2 lors de la phase LTO : {moteur.equivalent_carbone_LTO} g")
+#print(f"Emission de CO2 lors de la phase de croisière : {moteur.equivalent_carbone_seconde_cruise} g/s")
 
 #TEST AVION
-print(f"Le moteur de l'avion choisi est le moteur {avion.modele_moteur}")
-print(f"Il y a {avion.nombre_moteur} moteur sur cet avion")
+#print(f"Le moteur de l'avion choisi est le moteur {avion.modele_moteur}")
+#print(f"Il y a {avion.nombre_moteur} moteur sur cet avion")
 
 #TEST VOYAGE 
-print(voyage.distance_travel())
+#print(voyage.distance_travel())
+
+
 
 def interface_utilisateur():
-    print('BIENVENUE, voici un programme de responsabilité écologique et social du voyageur aéronautique ')
+    print('\n BIENVENUE, voici un programme de responsabilité écologique et social du voyageur aéronautique ')
     print("\n Il vous permet de comparer l'impact écologique global par passager des différentes compagnies anglaises")
     print("\n Si vous prévoyez un voyage en avion (en Angleterre), vous pouvez également comparer l'impact écologique de différents vols")
-    print("\n Veuillez remplir le fichier  'Donnees.yaml' afin de communiquer les entrées au programme puis appuyer sur ENTRER")
+    print(input("\n Veuillez remplir le fichier  'Donnees.yaml' afin de communiquer les entrées au programme puis appuyer sur ENTRER"))
 
+    Entrees_yaml = Lecteuryamel.lecteuryamel()
+    Donnees_dict = Entrees_yaml.print_content()
+
+    if input("\n Validez-vous ces données ? (OUI/NON)") == 'OUI':
+        return Donnees_dict
+    else:
+        print("\n Veuillez remplir le fichier 'Donnees.yaml' comme vous le souhaiter et réessayer")
+        sys.exit()
+
+Donnees_dict = interface_utilisateur()
+print(Donnees_dict)
